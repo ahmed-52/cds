e83cc03289cd8dd19ad6818d97690cf8202d4f37
Ahmed Abdulla <aaa384@cornell.edu>
2025-04-17 21:47:00 -0400

    added jibran and neha

--- frontend/src/components/TextGenerate.js
+++
@@ -1,8 +0,0 @@
-"use client";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
-
-const words = `----`;
-
-export function TextGenerate() {
-  return <TextGenerateEffect words={words} />;
-}
